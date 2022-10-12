5bcd7e7f55412747207446dbc6e68d4345235979
Janata Lab <janatalab@gmail.com>
2022-10-11 17:54:31 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from .settings import GEM_SETTINGS
 
 class ExperimentInitForm(forms.Form):
-    slaves_requested = forms.IntegerField(initial=1, validators=[MinValueValidator(1), MaxValueValidator(GEM_SETTINGS['max_tappers'])])
+    tappers_requested = forms.IntegerField(initial=1, validators=[MinValueValidator(1), MaxValueValidator(GEM_SETTINGS['max_tappers'])])
 
     metronome_alpha = forms.JSONField()
     metronome_tempo = forms.FloatField(initial=120.0)   #units: beats-per-minute
