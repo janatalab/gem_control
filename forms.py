cfcb36db42ef57bf0bc9a9eefb0bef0e79afbcce
Petr Janata <pjanata@ucdavis.edu>
2023-05-29 18:44:59 +0000

    removed unnecessary parameters from ExperimentInitForm

--- forms.py
+++ forms.py
@@ -8,10 +8,10 @@ from .settings import GEM_SETTINGS
 class ExperimentInitForm(forms.Form):
     tappers_requested = forms.IntegerField(initial=1, validators=[MinValueValidator(1), MaxValueValidator(GEM_SETTINGS['max_tappers'])])
 
-    metronome_alpha = forms.JSONField()
-    metronome_tempo = forms.FloatField(initial=120.0)   #units: beats-per-minute
-    repeats = forms.IntegerField(initial=10, validators=[MinValueValidator(1)])    #number of rounds at each alpha
-    windows = forms.IntegerField(initial=26, validators=[MinValueValidator(1)])    #number of metronome clicks; Fairhurst = 24
+    # metronome_alpha = forms.JSONField()
+    # metronome_tempo = forms.FloatField(initial=120.0)   #units: beats-per-minute
+    # repeats = forms.IntegerField(initial=10, validators=[MinValueValidator(1)])    #number of rounds at each alpha
+    # windows = forms.IntegerField(initial=26, validators=[MinValueValidator(1)])    #number of metronome clicks; Fairhurst = 24
     audio_feedback = forms.ChoiceField(choices=GEM_SETTINGS['feedback_options'])
 
     trial_generator = forms.CharField(initial='fully_random')  # either a keyword string, e.g. 'fully_random', or a module and method for generating a trial list
